d5a19ded634202aa472f43a7d1ed845995814bd6
蓝染 <lanran@lanrandeMacBook-Air.local>
2025-08-03 22:03:00 +0800

    浅色主题0.6 网格

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
 export const AUTHOR_NAME = "蓝染";
+export const CONTACT_EMAIL = "lanran408@gmail.com";
+export const GITHUB_URL = "https://github.com/kotvaer";
+export const X_URL = "https://x.com";
+export const YOUTUBE_URL = "https://youtube.com/";
